ff342589e7ed47c1ba794fad0536a39ba098db76
xianqing <wxqwxq@qq.com>
2018-04-25 13:11:30 +0800

    update

--- src/index/redux/saga/page2.js
+++ src/index/redux/saga/page2.js
@@ -13,8 +13,6 @@ import {
 // 一个工具函数：返回一个 Promise，这个 Promise 将在 1 秒后 resolve
 export const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-
-
 function* incremen(action) {
     yield delay(1000)
     yield put({type: PAGE2_INCREMEN, list: []});
